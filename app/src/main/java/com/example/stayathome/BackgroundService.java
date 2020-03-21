bc6a1916f5abf43262345a92f30c74fa9554a337
ubgzq <daniel.scheible@student.kit.edu>
2020-03-21 10:13:48 +0100

    Added functionality of wifi change detection

--- app/src/main/java/com/example/stayathome/BackgroundService.java
+++ app/src/main/java/com/example/stayathome/BackgroundService.java
@@ -6,14 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import java.time.Instant;
+
 public class BackgroundService extends Service {
     private BroadcastReceiver rec;
-    SharedPreferences pref;
+    private Context mainContext;
+
+    public BackgroundService(Context mainContext){
+        // Constructor
+        this.mainContext = mainContext;
+    }
 
     @Nullable
     @Override
@@ -26,27 +34,64 @@ public class BackgroundService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        this.rec = new WifiBroadcasts();
+        this.rec = new WifiBroadcasts(mainContext);
         IntentFilter filter = new IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION);
         this.registerReceiver(this.rec, filter);
-        //TODO: finish access to SharedPreferences
-        //pref = this.getSharedPreferences();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        unregisterReceiver(this.rec);
+        this.unregisterReceiver(this.rec);
     }
 } // End class BackgroundService
 
 
 class WifiBroadcasts extends BroadcastReceiver {
-    private static final String TAG = "WiFiBroadcasts";
+    private Context mainContext;
+    SharedPreferences prefs;
+
+    public WifiBroadcasts(Context mainContext){
+        this.mainContext = mainContext;
+        prefs = this.mainContext.getSharedPreferences(this.mainContext.getResources().getString(R.string.shared_prefs), Context.MODE_PRIVATE);
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)){
+            WifiManager wifiMgr = (WifiManager) mainContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
+            if(wifiMgr.isWifiEnabled()){
+                //Wi-Fi adapter is enabled (on)
+                WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+
+                if(wifiInfo.getNetworkId() != -1){
+                    // Connected to an access point
+                    String savedSSID = prefs.getString("wifi_name", "");
+                    String savedBSSID = prefs.getString("wifi_id", "");
+
+                    if(wifiInfo.getSSID().equals(savedSSID)){
+                        // SSID is the same as the one that the user saved
+                        updateWifiConnectedTime();
+                    } else if(wifiInfo.getBSSID().equals(savedBSSID)) {
+                        // SSID is not the same, but MAC-address did not change -> update SSID
+                        prefs.edit().putString("wifi_name", wifiInfo.getSSID()).apply();
+                        updateWifiConnectedTime();
+                    } else{
+                        // Completely different network
+                    }
+
+                } else {
+                    // Not connected to an access point
+                }
+            } else {
+                //Wi-Fi adapter is disabled (off)
+            }
+        }
+    }
+
+    private void updateWifiConnectedTime(){
+        // Instant instant = Instant.now();
     }
 } // End class WiFiBroadcasts
