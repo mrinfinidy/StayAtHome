8ddc46a3382e4cddba568367fdfcf58c24b4fc7d
ubgzq <daniel.scheible@student.kit.edu>
2020-03-21 11:52:05 +0100

    Initial commit

---
+++ app/src/test/java/com/example/stayathome/BackgroundServiceTest.java
@@ -0,0 +1,10 @@
+package com.example.stayathome;
+
+import org.junit.Test;
+
+public class BackgroundServiceTest {
+    @Test
+    public void testSystemTime(){
+        //WifiBroadcasts.updateWifiConnectedTime();
+    }
+}
