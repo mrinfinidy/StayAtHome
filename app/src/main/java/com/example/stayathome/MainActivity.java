46beebabfbb7500b26b98709f4b5034da986032a
afkdev8 <afkdev8@gmail.com>
2020-03-21 02:24:44 +0100

    Initial Setup: Screen that only appears upon installation. Store ssid & bssid when wifi gets confirmed.

--- app/src/main/java/com/example/stayathome/MainActivity.java
+++ app/src/main/java/com/example/stayathome/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.stayathome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //check opened for first time
+        SharedPreferences firstUsage = getSharedPreferences("firstUsage", MODE_PRIVATE);
+        boolean isFirstUsage = firstUsage.getBoolean("first_usage", true);
+
+
+        if (isFirstUsage) {
+            //first usage
+            SharedPreferences.Editor firstTimeEditor = firstUsage.edit();
+            firstTimeEditor.putBoolean("first_usage", false);
+            firstTimeEditor.apply();
+
+            //initial setup screen
+            Intent firstTime = new Intent(MainActivity.this, InitialSetup.class);
+            startActivity(firstTime);
+
+        } else {
+            //regular execution
+
+
+
+        }
     }
+
 }
