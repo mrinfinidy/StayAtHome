dd40728d4028647235ee16add18b4ccc5c9ed1a7
ubgzq <daniel.scheible@student.kit.edu>
2020-03-21 17:26:42 +0100

    First implementation of location permission request

--- app/src/main/java/com/example/stayathome/InitialSetup.java
+++ app/src/main/java/com/example/stayathome/InitialSetup.java
@@ -1,8 +1,12 @@
 package com.example.stayathome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -14,6 +18,7 @@ import com.example.stayathome.helper.SharedPreferencesHelper;
 
 public class InitialSetup extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 1;
     SharedPreferencesHelper prefHelper;
 
     @Override
@@ -24,7 +29,12 @@ public class InitialSetup extends AppCompatActivity {
         // Assign SharedPreferencesHelper when the Activity is created
         prefHelper = new SharedPreferencesHelper(this);
 
-        //save SSID when 'wifi betaetigen' is pressed
+        // Request permission for location in order to receive the name of the SSID and BSSID
+        while(!locationPermissionGranted()){
+            requestLocationPermission();
+        }
+
+        // Save SSID when 'wifi betaetigen' is pressed
         Button confirmWifi = (Button) findViewById(R.id.confirmWifi);
         confirmWifi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,4 +65,30 @@ public class InitialSetup extends AppCompatActivity {
         super.finish();
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
-}
+
+    private boolean locationPermissionGranted(){
+        // Return true if access to the location permission has been granted
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    break;
+                } else {
+                    // Permission denied
+                    break;
+                }
+            }
+            // Add more cases to the switch to check for other permission that might have been requested
+        }
+    }
+} // End class InitialSetup
