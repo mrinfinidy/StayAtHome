12cd39a49c97f0527510d97cf811d30421ce76c5
afkdev8 <afkdev8@gmail.com>
2020-03-22 07:22:54 +0100

    Screen to choose project (location)

--- app/src/main/java/com/example/stayathome/ChooseProject.java
+++ app/src/main/java/com/example/stayathome/ChooseProject.java
@@ -1,14 +1,37 @@
 package com.example.stayathome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ChooseProject extends AppCompatActivity {
+
+    RecyclerView locationsRecView;
+    RecyclerView.LayoutManager locationsLayout;
+    RecyclerView.Adapter locationsAdapter;
+    ArrayList<String> locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_project);
+
+        locationsRecView = (RecyclerView) findViewById(R.id.locationsRecView);
+
+        locations = new ArrayList<>();
+        locations.add("Karlsruhe");
+        locations.add("Jena");
+        locations.add("Weimar");
+        locations.add("Frankfurt");
+        locations.add("Berlin");
+
+        locationsLayout = new LinearLayoutManager(this);
+        locationsAdapter = new MainAdapter(locations);
+        locationsRecView.setLayoutManager(locationsLayout);
+        locationsRecView.setAdapter(locationsAdapter);
     }
 }
---
+++ app/src/main/java/com/example/stayathome/MainAdapter.java
@@ -0,0 +1,49 @@
+package com.example.stayathome;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    ArrayList<String> locations;
+
+    public MainAdapter(ArrayList<String> locations) {
+        this.locations = locations;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.location_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainAdapter.ViewHolder holder, int position) {
+        holder.locationName.setText(locations.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return locations.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView locationName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            locationName = itemView.findViewById(R.id.location_name);
+        }
+    }
+}
