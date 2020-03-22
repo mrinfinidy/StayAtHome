95fa311b4ff456b312ce58ddf1675ffc2bb379aa
ubgzq <daniel.scheible@student.kit.edu>
2020-03-22 08:55:47 +0100

    Added some comments

--- app/src/main/java/com/example/stayathome/helper/NotificationHelper.java
+++ app/src/main/java/com/example/stayathome/helper/NotificationHelper.java
@@ -10,10 +10,6 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.example.stayathome.R;
 
-/**
- * @author Daniel Scheible, created on 21.03.2020
- */
-
 public class NotificationHelper {
 
     public static final String CHANNEL_ID_GROWTH_PROGRESS = "100";
@@ -28,18 +24,23 @@ public class NotificationHelper {
             channel.setDescription(description);
             // Register the channel with the system
             NotificationManager notManager = mainContext.getSystemService(NotificationManager.class);
-            notManager.createNotificationChannel(channel);
+            if(notManager != null)
+                notManager.createNotificationChannel(channel);
         }
     }
 
     public static void sendNotification(Context context, String channelID, String title, String text){
+        // Build the notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelID);
         builder.setSmallIcon(R.drawable.ic_launcher_foreground);
         builder.setContentTitle(title);
         builder.setContentText(text);
         builder.setStyle(new NotificationCompat.BigTextStyle().bigText(text));
         builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        //builder.setContentIntent();
         builder.setAutoCancel(true);
+
+        // Show the notification
         NotificationManagerCompat notManager = NotificationManagerCompat.from(context);
         notManager.notify(100, builder.build());
     }
