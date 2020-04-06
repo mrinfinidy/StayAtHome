237cb8665c0107e1286760e54ef16f28ef8d39e5
mr.infinidy <afkdev8@gmail.com>
2020-04-06 06:16:43 +0200

    grown virtual trees display

--- app/src/main/java/com/example/stayathome/ui/GrownTrees.java
+++ app/src/main/java/com/example/stayathome/ui/GrownTrees.java
@@ -2,6 +2,7 @@ package com.example.stayathome.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
@@ -24,9 +25,10 @@ import java.util.concurrent.ExecutionException;
 
 public class GrownTrees extends AppCompatActivity {
 
-    int numGrownTrees;
-    SharedPreferencesHelper prefHelper;
+    private SharedPreferencesHelper prefHelper;
+    private WifiManager wifiManager;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onResume() {
         super.onResume();
@@ -35,17 +37,27 @@ public class GrownTrees extends AppCompatActivity {
         // Assign SharedPreferencesHelper when the Activity is created
         prefHelper = new SharedPreferencesHelper(this);
 
-        numGrownTrees = prefHelper.retrieveInt("grown_trees_virtual");
+        //# of trees in wifi
+        int numGrownTrees = 0;
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        try {
+            numGrownTrees = MainActivity.treeInfo.treesInWifi(wifiManager.getConnectionInfo().getSSID()).size();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         TextView currentVirtualTrees = findViewById(R.id.currentVirtualTrees);
         currentVirtualTrees.setText(numGrownTrees + "");
+        prefHelper.storeInt("grown_trees_virtual", numGrownTrees);
 
         TextView vTreesDisplay = findViewById(R.id.vTreesDisplay);
-        showTrees(prefHelper, vTreesDisplay);
+        showTrees(wifiManager, vTreesDisplay);
     }
 
     public void plantTree(View v) {
         //retrieve # of already grown virtual trees
-        numGrownTrees = prefHelper.retrieveInt("grown_trees_virtual");
+        int numGrownTrees = prefHelper.retrieveInt("grown_trees_virtual");
 
         //max. # of virtual trees
         int virtualTreesLimit = 2;
@@ -64,8 +76,7 @@ public class GrownTrees extends AppCompatActivity {
     }
 
     //show names grown virtual trees
-    private void showTrees(SharedPreferencesHelper prefHelper, TextView vTreesDisplay) {
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE); //currently connected wifi
+    private void showTrees(WifiManager wifiManager, TextView vTreesDisplay) {
         List<Tree> treesInWifi = new ArrayList<Tree>();
 
         //get list of trees in this wifi
